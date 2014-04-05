bd79699063aec6aba1b6e5d6f138ebb0b283e6fc
David Soulayrol <david.soulayrol@gmail.com>
2014-04-05 11:30:18 +0200

    Improved the Settings screen, with dynamic summary.

--- sholi/src/main/java/name/soulayrol/rhaa/sholi/SettingsFragment.java
+++ sholi/src/main/java/name/soulayrol/rhaa/sholi/SettingsFragment.java
@@ -18,15 +18,59 @@
  */
 package name.soulayrol.rhaa.sholi;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 
 
-public class SettingsFragment extends PreferenceFragment {
+public class SettingsFragment extends PreferenceFragment
+        implements SharedPreferences.OnSharedPreferenceChangeListener {
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.preferences);
+        PreferenceManager.setDefaultValues(getActivity(), R.xml.preferences, false);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        SharedPreferences sharedPreferences =
+                PreferenceManager.getDefaultSharedPreferences(getActivity());
+
+        getPreferenceScreen().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+        updatePreferenceSummary(sharedPreferences, SettingsActivity.KEY_LIST_ITEM_SIZE);
+        updatePreferenceSummary(sharedPreferences, SettingsActivity.KEY_IMPORT_MERGE_POLICY);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        getPreferenceScreen().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        updatePreferenceSummary(sharedPreferences, key);
+    }
+
+    private void updatePreferenceSummary(SharedPreferences sharedPreferences, String key) {
+        if (key.equals(SettingsActivity.KEY_LIST_ITEM_SIZE)) {
+            Preference p = findPreference(key);
+            p.setSummary(sharedPreferences.getString(key, ""));
+        }
+        else if (key.equals(SettingsActivity.KEY_IMPORT_MERGE_POLICY)) {
+            ListPreference p = (ListPreference) findPreference(key);
+            p.setSummary(p.getEntry());
+        }
     }
 }
